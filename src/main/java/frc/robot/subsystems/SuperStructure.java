eae64dff493ea2d9bb2eac769b3f8266177cd62b
Michael M <mmarchiniakjr@hotmail.com>
2025-10-25 11:42:45 -0500

    Add initial project configuration and arm subsystem implementation

---
+++ src/main/java/frc/robot/subsystems/SuperStructure.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SuperStructure extends SubsystemBase {
+  
+  private SuperStructureState currentState = SuperStructureState.DEFAULT_STATE;
+  private SuperStructureState desiredState = SuperStructureState.DEFAULT_STATE;
+  private SuperStructureStatus currentStatus = SuperStructureStatus.IDLE;
+
+  public enum SuperStructureState {
+      DEFAULT_STATE,
+      INTAKE_UPRIGHT,
+      INTAKE_UPSIDE_DOWN,
+      HOLDING,
+      OUTTAKING,
+  }
+  public enum SuperStructureStatus {
+      IDLE,
+      TRANSITIONING,
+      ERROR,
+  }
+
+  public void setDesiredState(SuperStructureState state) {
+      if (state != currentState) {
+          currentStatus = SuperStructureStatus.TRANSITIONING;
+      }
+      this.desiredState = state;
+  }
+  public SuperStructureState getDesiredState() {
+      return this.desiredState;
+  }
+  public SuperStructureState getCurrentState() {
+      return this.currentState;
+  }
+  public boolean atDesiredState() {
+      return (this.currentState == this.desiredState) && (this.currentStatus == SuperStructureStatus.IDLE);
+  }
+
+  /** Creates a new SuperStructure. */
+  public SuperStructure() {}
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.arm;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ArmIO {
+  @AutoLog
+  public static class ArmIOInputs {
+    public double leftPositionRad = 0.0;
+    public double leftVelocityRadPerSec = 0.0;
+    public double leftAppliedVolts = 0.0;
+    public double leftCurrentAmps = 0.0;
+
+    public double rightPositionRad = 0.0;
+    public double rightVelocityRadPerSec = 0.0;
+    public double rightAppliedVolts = 0.0;
+    public double rightCurrentAmps = 0.0;
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(ArmIOInputs inputs) {}
+
+  /** Run open loop at the specified voltage. */
+  public default void setVoltage(double leftVolts, double rightVolts) {}
+
+  /** Run closed loop at the specified velocity. */
+  public default void setVelocity(
+      double leftRadPerSec, double rightRadPerSec, double leftFFVolts, double rightFFVolts) {}
+}
